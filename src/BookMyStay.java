774b612e4ea7cad262b4e4f100d7c2b065dcfdb4
jushiyagrover21-glitch <jushiyagrover21@gmail.com>
2026-03-13 10:02:16 +0530

    UC2

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,3 +1,27 @@
-public class BookMyStay {
+/**
+ * Abstract Room class representing common properties of all room types.
+ *
+ * @author Jushi
+ * @version 2.0
+ */
+public abstract class Room {
 
+    protected String roomType;
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(String roomType, int beds, int size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq ft");
+        System.out.println("Price per night: $" + price);
+    }
 }
